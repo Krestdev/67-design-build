841dd3adcb082a5f89fdac84e0752c7e3326da73
Hansjason97 <hansadiogo@gmail.com>
2025-11-07 23:58:16 +0100

    smoothing the animation

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -51,7 +51,9 @@ function Page() {
         <OnViewAnimation className='z-10' delay={0.55}>
           <Link href={"/contact"} className='z-10'><Button size={"lg"}>{t("mainbutton")}<ChevronRight size={24}/></Button></Link>
         </OnViewAnimation>
-        <img src={"/images/hero.webp"} alt="hero" className="absolute top-0 sm:top-1/2 left-1/2 -translate-x-1/2 translate-y-0 sm:-translate-y-1/2 rounded-none sm:rounded-2xl md:rounded-3xl lg:rounded-4xl xl:rounded-[40px] w-full sm:w-[calc(100vw-40px)] h-full sm:h-[calc(100%-40px)] object-cover"/>
+        <OnViewAnimation duration={1.5} animation="popIn" className="absolute top-0 sm:top-1/2 left-1/2 -translate-x-1/2 translate-y-0 sm:-translate-y-1/2 w-full sm:w-[calc(100vw-40px)] h-full sm:h-[calc(100%-40px)]">
+          <img src={"/images/hero.webp"} alt="hero" className="w-full h-full object-cover rounded-none sm:rounded-2xl md:rounded-3xl lg:rounded-4xl xl:rounded-[40px]"/>
+        </OnViewAnimation>
         <div className='absolute top-0 left-0 w-full h-full bg-linear-to-t from-white/10 via-white/70 to-white' />
       </section>
        {/**Hero section End */}
