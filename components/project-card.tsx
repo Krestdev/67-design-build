344c3f0812a1b9483189f3c43b5c28b11b06085f
Hansjason97 <hansadiogo@gmail.com>
2025-11-07 17:27:17 +0100

    project on homepage

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { Button } from './ui/button';
 import { ArrowUpRight } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import OnViewAnimation from './onViewAnimation';
 
 interface Props extends ProjectPreview {
     RTL: boolean;
@@ -11,25 +12,31 @@ interface Props extends ProjectPreview {
 }
 
 function ProjectCard({RTL=false, title, description, slug, images, style}:Props) {
+    const [first, second, third] = images;
     if(style === "default"){
         return (
           <div className="flex flex-col gap-5 sm:gap-8">
             <Link href={`/projets#${slug}`} target="_blank" className={cn("flex justify-between gap-3 flex-wrap")}>
-                <div className="grid gap-1">
+                <OnViewAnimation animation="slideLeft" className="grid gap-1">
                     <h4 className="text-white">{title}</h4>
                     <p className="text-gray-300">{description}</p>
-                </div>
+                </OnViewAnimation>
                 <Button variant={"ghost"} size={"icon"} className="text-gray-200 hover:text-white">
                     <ArrowUpRight/>
                 </Button>
             </Link>
-            <div className={cn("flex gap-3", RTL ? "flex-row-reverse" : "flex-row")}>
-                <img src={images[0].src} alt={images[0].alt} className="basis-full sm:basis-2/3 h-auto rounded-[20px] aspect-video object-center" />
-                <div className="hidden sm:basis-1/3 sm:flex flex-col gap-3">
-                    {images.slice(1,3).map((image, id)=>(
-                        <img key={id} src={image.src} alt={image.alt} className="w-full h-auto rounded-[20px] aspect-video object-center"/>
-                    ))}
-                </div>
+            <div className={cn("grid grid-cols-1 sm:grid-cols-3 grid-rows-2 gap-3")}>
+                <OnViewAnimation animation="popIn" delay={0.25} className={cn("col-span-1 sm:col-span-2 row-span-2", RTL && "order-2")}>
+                    <img src={first.src} alt={first.alt} className={cn("w-full h-auto rounded-[20px] aspect-video object-center")} />
+                </OnViewAnimation>
+                <OnViewAnimation animation="popIn" delay={0.45} className={cn("hidden sm:inline-block",  RTL && "order-0")}>
+                    <img src={second.src} alt={second.alt} className={cn("w-full h-auto rounded-[20px] aspect-video")} />
+                </OnViewAnimation>
+                <OnViewAnimation animation="popIn" delay={0.65} className={cn("hidden sm:inline-block",  RTL && "order-3")}>
+                    <img src={third.src} alt={third.alt} className={cn("hidden sm:inline-block w-full h-auto rounded-[20px] aspect-video", RTL && "order-3")} />
+                </OnViewAnimation>
+                
+                
             </div>
           </div>
         )
